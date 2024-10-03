f2f1f1b3b2ab1ec5f2bb8a8d4019636796e41829
Jaimito27 <softjaimito27@gmail.com>
2024-10-03 15:10:30 -0500

    Se crea interface de student y servicios donde se parametrizan los metodos del crud

--- src/main/java/com/example/crudspring/repository/StudentRepository.java
+++ src/main/java/com/example/crudspring/repository/StudentRepository.java
@@ -1,4 +1,11 @@
 package com.example.crudspring.repository;
 
-public class StudentRepository {
+import com.example.crudspring.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> { //le indico que extiendo de jpa repository y hago referencia a la entidad Student y el tipo de dato del campo primario
+
+
 }
