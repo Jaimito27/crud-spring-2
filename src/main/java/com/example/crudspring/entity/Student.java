4d150a6a9a31ba65352deb5c856260f74b4d67d8
Jaimito27 <softjaimito27@gmail.com>
2024-10-03 14:58:34 -0500

    Se cambian caracteristicas del propterties para cambiar propiedades de una tabla y se termina de configurar la entidad de student, ahora se crea repositorio de estudiantes

--- src/main/java/com/example/crudspring/entity/Student.java
+++ src/main/java/com/example/crudspring/entity/Student.java
@@ -1,7 +1,6 @@
 package com.example.crudspring.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data //ya no es obligatorio agregar get y set
@@ -9,5 +8,13 @@ import lombok.Data;
 @Entity
 @Table(name = "tbl_student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+
 
 }
